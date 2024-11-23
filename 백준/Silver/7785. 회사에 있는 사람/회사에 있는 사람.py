1626a079b8d78cc780198bbef8f5c88c607b3726
PARKSUJIN <107746547+ssuzn@users.noreply.github.com>
2024-11-23 19:46:23 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 240 ms, Memory: 134752 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,15 @@
+n = int(input())
+tmp = dict()
+
+for _ in range(n):
+    a, b = map(str, input().split())
+    
+    if b == "enter":
+        tmp[a] = b
+    else:
+        del tmp[a]
+        
+tmp = sorted(tmp.keys(), reverse=True)
+
+for i in tmp:
+    print(i)
